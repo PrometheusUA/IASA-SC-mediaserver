62114c99a7871bea62dff63d1b8baf75da563e3c
PrometheusUA <prometheusua@i.ua>
2022-01-07 20:23:38 +0200

    Minor: clean security folder

--- src/main/java/ua/kpi/iasa/sc/mediaserver/security/SecurityConfig.java
+++ src/main/java/ua/kpi/iasa/sc/mediaserver/security/SecurityConfig.java
@@ -3,24 +3,12 @@ package ua.kpi.iasa.sc.mediaserver.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import ua.kpi.iasa.sc.mediaserver.security.filter.CustomAuthorizationFilter;
 
 import java.util.Arrays;
 
@@ -30,14 +18,6 @@ import static org.springframework.http.HttpMethod.*;
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-//    private final UserDetailsService userDetailsService;
-//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-//    }
-
     @Bean
     protected CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -65,23 +45,5 @@ public class SecurityConfig {
                 .logout()
                 .disable()
                 .build();
-//        http.csrf().disable()
-//                .httpBasic().disable()
-//                .logout().disable()
-//                .formLogin().disable()
-//                .cors().and()
-//                .authorizeExchange()
-//                .pathMatchers(POST, "/video").hasAnyAuthority("Admin")
-//                .pathMatchers("/**").permitAll();
-//
-//        http.authorizeExchange().pathMatchers(POST, "/video").hasAnyAuthority("Admin");
-//        http.addFilterAt(new CustomAuthorizationFilter(), SecurityWebFiltersOrder.AUTHORIZATION);
-//        return http.build();
     }
-
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception{
-//            return super.authenticationManagerBean();
-//    }
 }
